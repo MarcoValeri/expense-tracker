05ac29da201db9187a7c05b7d98ba67a13ddf5cf
MarcoValeri <marcovaleri@hotmail.it>
2023-07-28 16:27:29 +0100

    Add button

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
@@ -5,13 +7,22 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
+    const [title, setTitle] = useState(props.title);
+
+    // Events
+    const clickHandler = () => {
+        console.log(`clickHandler`);
+        setTitle("Updated");
+    }
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
