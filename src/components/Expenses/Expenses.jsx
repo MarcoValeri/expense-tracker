c27adedaeeb71e111557d8c016fedf728d5363f8
MarcoValeri <marcovaleri@hotmail.it>
2023-08-09 16:25:07 +0100

    Improve expenses component

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 
 import './Expenses.css';
 
@@ -18,23 +18,13 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   })
 
-  let expensesContent = <p>No expenses found.</p>;
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => {
-        return <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-        />
-    });
-  }
+
 
     return (
         <div>
           <Card className="expenses">
               <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-              {expensesContent}
+              <ExpensesList items={filteredExpenses} />
           </Card>
         </div>
     )
